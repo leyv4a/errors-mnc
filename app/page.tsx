734f0c701d3771a68991b93a152949ce9d7066d6
leyv4a <gabriel.leyva228902@potros.itson.edu.mx>
2024-08-29 13:17:53 -0700

    feat: add errors solution dialog

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,8 @@ import { Button } from "@/components/ui/button";
 import React from "react";
 import {
   Dialog,
+  DialogClose,
   DialogContent,
-  DialogDescription,
-  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
@@ -15,13 +14,17 @@ import { Input } from "@/components/ui/input";
 import { History, CircleHelp } from "lucide-react";
 
 export default function Home() {
+
+  // Campos para guardar los valores del formulario
   const [real, setReal] = React.useState<string>("");
   const [aprox, setAprox] = React.useState<string>("");
 
+  // Campos para guardar el resultado de la funcion calcular errores
   const [absoluteError, setAbsoluteError] = React.useState<string>("");
   const [relativeError, setRelativeError] = React.useState<string>("");
   const [porcentualError, setPorcentualError] = React.useState<string>("");
 
+  // Controlador para validar el cambio en los inputs
   const handleInputChange =
     (setter: React.Dispatch<React.SetStateAction<string>>) =>
     (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -36,6 +39,7 @@ export default function Home() {
       }
     };
 
+    // Estilos en cascada para usar nuestro fondo personalizado
   const backgroundStyle = {
     backgroundImage: 'url("/starsky.gif")',
     backgroundSize: "cover",
@@ -43,14 +47,21 @@ export default function Home() {
     backgroundPosition: "center",
   };
 
+  // Función asíncrona para manejar la peticion del usuario
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     //setIsLoading(true)
     try {
+      // Validamos que haya campos
+      if (!real || !aprox) {
+        return;
+      }
+      // Guardamos los errores
       const errors = await calculateErrors(
         parseFloat(aprox) || 0,
         parseFloat(real) || 0
       );
+      // Asignamos los errores a sus campos
       setAbsoluteError(errors.Absolute);
       setRelativeError(errors.Relative);
       setPorcentualError(errors.Porcentage);
@@ -60,19 +71,19 @@ export default function Home() {
     }
   };
 
+  // VISTA PRINCIPAL
   return (
     <main
       style={backgroundStyle}
       className="flex min-h-screen h-screen flex-col items-center justify-between pt-10 pb-24 px-24"
     >
       <h1 className="text-white text-5xl font-bold mb-10 font-nerko">
-        {" "}
         Calculadora de errores
       </h1>
       <section className="bg-slate-800 h-full  p-5 rounded flex gap-2">
         <form
           onSubmit={handleSubmit}
-          className="bg-slate-500 p-3 rounded flex flex-col gap-2 "
+          className="bg-slate-500 max-w-[300px] w-[300px] p-3 rounded flex flex-col gap-2 "
         >
           <Input
             value={real}
@@ -87,43 +98,27 @@ export default function Home() {
           <Button type="submit" size={"sm"}>
             Calcular
           </Button>
-          <article className="pt-5 text-center ">
-            <h2 className="text-slate-800 font-bold"> Resultados : </h2>
+          <article className={`pt-5 text-center ${absoluteError.trim() != '' ? '' : 'hidden'}`}>
+            <h2 className={`text-slate-800 font-bold`}> Resultados : </h2>
             <div className="rounded bg-slate-400 p-2 mt-2 text-start flex justify-between">
-              {/* <p className="text-sm">Formula:  <span>  |</span></p> */}
               <p className="text-xs">
                 {" "}
                 {/* Error absoluto: | x - x<sub>2</sub> | = x{" "} */}
                 Error absoluto: | {real} - {aprox} | = {absoluteError}
               </p>
-              {/* ESTE QUIERO QUE SEA EL TRIGGER DEL DIALOG PARA MOSTRAR EL PROCEDIMIENTO DE LA FORMULA */}
-              {/* <Button className="size-4 hover:bg-slate-400" variant={'ghost'} size={'icon'}><CircleHelp/></Button> */}
-              <DialogHelp real={1} aprox={2} type="absoluto" />
+              <DialogHelp type="absoluto" real={parseFloat(real)} aprox={parseFloat(aprox)} resultado={absoluteError as string}/>
             </div>
             <div className="rounded bg-slate-400 p-2 mt-2 text-start flex justify-between">
               <p className="text-xs">
                 {" "}
-                {/* Error relativo: <sup>ea</sup>&frasl;<sub>x</sub> = x{" "} */}
                 Error relativo: <sup>{absoluteError}</sup>&frasl;<sub>{real}</sub> = {relativeError}
               </p>
-              <Button
-                className="size-4 hover:bg-slate-400"
-                variant={"ghost"}
-                size={"icon"}
-              >
-                <CircleHelp />
-              </Button>
+              <DialogHelp type="relativo" real={parseFloat(absoluteError)} aprox={parseFloat(real)} resultado={relativeError as string}/>
             </div>
             <div className="rounded bg-slate-400 p-2 mt-2 text-start flex justify-between">
               {/* <p className="text-xs"> Error porcentual: er × 100 = x% </p> */}
               <p className="text-xs"> Error porcentual: {relativeError} × 100 = {porcentualError}% </p>
-              <Button
-                className="size-4 hover:bg-slate-400"
-                variant={"ghost"}
-                size={"icon"}
-              >
-                <CircleHelp />
-              </Button>
+              <DialogHelp type="porcentual" real={parseFloat(relativeError)} resultado={porcentualError as string}/>
             </div>
           </article>
         </form>
@@ -135,7 +130,7 @@ export default function Home() {
           >
             <History />
           </Button>
-          <aside className=" flex flex-col  ">
+          <aside className="flex flex-col">
             <div>
               <HistoryCard />
             </div>
@@ -150,8 +145,12 @@ type Props = {
   type: string;
   real?: number;
   aprox?: number;
+  resultado? : string
 };
-function DialogHelp({ real, aprox }: Props) {
+import { ThumbsUp } from 'lucide-react';
+
+// Dialogo que nos mostrara la ayuda para los calculs
+function DialogHelp({type, real, aprox, resultado} : Props) {
   return (
     <>
       <Dialog>
@@ -164,34 +163,42 @@ function DialogHelp({ real, aprox }: Props) {
             <CircleHelp />
           </Button>
         </DialogTrigger>
-        <DialogContent className="sm:max-w-[425px]">
+        <DialogContent className="sm:max-w-[425px] bg-slate-900 border-0 text-slate-300">
           <DialogHeader>
-            <DialogTitle>Edit profile</DialogTitle>
-            <DialogDescription>{real}</DialogDescription>
+            <DialogTitle>Explicación cálculo de error {type}</DialogTitle>
           </DialogHeader>
-          <div className="grid gap-4 py-4">
-            <div className="grid grid-cols-4 items-center gap-4">
-              {aprox}
-              <Input id="name" value="Pedro Duarte" className="col-span-3" />
+            <div className="flex gap-2 text-xs">
+              <p> Valor real: <span className="font-bold">{real}</span></p>  <p>Valor aproximado: <span className="font-bold">{aprox}</span></p>
             </div>
-            <div className="grid grid-cols-4 items-center gap-4">
-              Username
-              <Input id="username" value="@peduarte" className="col-span-3" />
+            <div>
+              {type == 'absoluto' ? 
+                <>
+                <p className="text-sm mb-2">Formúla: | valor real - valor aprox | = Error absoluto</p>
+                <p className="text-sm">Sustitución y resultado: | {real} - {aprox} | = {resultado}</p>
+                </>
+              : type == 'relativo' ?
+              <>
+                <p className="text-sm mb-2">Formúla: <sup>error absoluto</sup>&frasl;<sub>valor real</sub> = Error relativo</p>
+                <p className="text-sm">Sustitución y resultado: <sup>{real}</sup>&frasl;<sub>{aprox}</sub> = {resultado}</p>
+              </>
+              :
+              <>
+              <p className="text-sm mb-2">Formúla: Error porcentual = (error relativo × 100)</p>
+              <p className="text-sm">Sustitución y resultado: {real} × 100 = {resultado}%</p>
+              </>
+              }
             </div>
-          </div>
-          <DialogFooter>
-            <Button type="submit">Save changes</Button>
-          </DialogFooter>
+
+          <DialogClose asChild>
+            <Button size={'icon'} className="bg-transparent" type="submit"><ThumbsUp/></Button>
+          </DialogClose>
         </DialogContent>
       </Dialog>
     </>
   );
 }
 
-type AbsoluteProps = {
-  aprox: number;
-  real: number;
-};
+// Funcion que calculara los errores
 const calculateErrors = async (aprox: number, real: number) => {
   return {
     Absolute: Math.abs(aprox - real).toFixed(4),
